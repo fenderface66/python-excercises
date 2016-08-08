da20f42b289098021043d66f082e95af1e594b4e
Robbie Heygate <r.heygate@gmail.com>
2016-08-08 17:27:17 +0100

    Added GUI to Ext Rename

--- ExtRename/ExtRename.py
+++ ExtRename/ExtRename.py
@@ -1,22 +1,38 @@
+from Tkinter import *
 import Tkinter, tkFileDialog, os,sys
 from distutils.core import setup
 import py2exe
 
 root = Tkinter.Tk()
 dirname = tkFileDialog.askdirectory(parent=root,initialdir="/",title='Please select a directory')
-if len(dirname ) > 0:
+
+def extReplace():
+  global e1
+  global e2
   folder = dirname
-  print "You chose %s" % folder
-  oldExt = raw_input("What extension type would you like to replace?")
-  newExt = raw_input("What would you like the new extension to be?")
+  oldExt = e1.get()
+  newExt = e2.get()
   for filename in os.listdir(folder):
     print filename
     oldbase = os.path.splitext(filename)
-  
+
     infilename = os.path.join(folder,filename)
     oldbase = os.path.splitext(filename)
     newname = infilename.replace(oldExt, newExt)
     output = os.rename(infilename, newname)
 
+label1 = Label( root, text="Old File Ext")
+e1 = Entry(root, bd =5)
+label2 = Label( root, text="New File Ext")
+e2 = Entry(root, bd =5)
 
-setup(console=['test.py'])
+label1.pack()
+e1.pack()
+label2.pack()
+e2.pack()
+submit = Button(root, text ="Submit", command = extReplace)
+submit.pack(side =BOTTOM)    
+    
+if len(dirname ) > 0:
+  
+  root.mainloop()
